913afc9610238847af5a882ceb14606e90f3e9af
farahShabbirButt <farashabbirbutt99@gmail.com>
2025-12-25 21:57:36 +0500

    Subject Module - Dto added - WIP

---
+++ src/modules/subject/subject.types.ts
@@ -0,0 +1,35 @@
+import { SubjectType } from '@prisma/client';
+import { IBaseEntity } from 'common/entities/base.js';
+import { v4 as uuid } from 'uuid';
+
+export interface ISubject extends IBaseEntity {
+  name: string;
+  subjectType: SubjectType;
+  maxMarks: number;
+  isActive: boolean;
+}
+export interface ICreateSubject {
+  name: string;
+  subjectType: SubjectType;
+  maxMarks: number;
+  isActive?: boolean;
+  createdBy?: string;
+}
+
+export interface IUpdateSubject {
+  name?: string;
+  subjectType?: SubjectType;
+  maxMarks?: number;
+  isActive?: boolean;
+  updatedBy?: string;
+}
+export function createSubjectMapper(payload: ICreateSubject): Partial<ISubject> {
+  return {
+    externalId: uuid(),
+    name: payload.name,
+    subjectType: payload.subjectType,
+    maxMarks: payload.maxMarks,
+    isActive: payload?.isActive ?? true,
+    createdBy: payload.createdBy ?? null,
+  };
+}
