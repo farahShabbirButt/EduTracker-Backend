9f42f022e8f2679e6b32b0874ba9f1c8ff7888b7
farahShabbirButt <farashabbirbutt99@gmail.com>
2026-01-11 19:11:53 +0500

    remove use of console

--- src/app.ts
+++ src/app.ts
@@ -15,11 +15,6 @@ const BASE = APP_BASE + API_ROUTE;
 const SUBJECT = BASE + '/subject';
 const SUBJECT_CLASS = BASE + '/subject-class';
 
-app.use((req, _res, next) => {
-  console.info('➡️ Incoming:', req.method, req.url);
-  next();
-});
-
 // Global middlewares
 app.use(cors());
 app.use(express.json());
